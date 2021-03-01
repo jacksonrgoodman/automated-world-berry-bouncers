8f1477a2bc7efda90272d6365ec23a0b6d37a91d
ColtMay <colten_mayberry@pm.me>
2021-03-01 15:13:25 -0600

    wrote necessary javascript for celebrities

---
+++ scripts/iceland/celebrities/Celebrities.js
@@ -0,0 +1,11 @@
+// Function that returns people passed through as rendered HTML elements
+export const icelandPeople = (icelandPeopleObject) => {
+    return(
+        `<article class ="people-card">
+            <div><img class="people-image" src ="./images/${icelandPeopleObject.img}" alt="${icelandPeopleObject.name}"/></div>
+            <h3 class="people-name">${icelandPeopleObject.name}</h3>
+            <p class="people-job">${icelandPeopleObject.job}</p>
+            <p class="people-bio">${icelandPeopleObject.bio}</p>
+        </article>`        
+    )
+} 
