2473067561463308bb73deb357ec8b1608b7ab75
JacksonRGoodman <jacksonrgoodman@gmail.com>
2021-03-02 15:55:58 -0600

    south korea css styling

--- scripts/southKorea/southKoreaLocations/southKoreaLocations.js
+++ scripts/southKorea/southKoreaLocations/southKoreaLocations.js
@@ -1,10 +1,10 @@
 export const southKoreaLandmarks = (southKoreaLandmarksObj) => {
     return(
-        `<article class="landmarks-card">
+        `<article class="people-card">
             <div><img class="landmarks-image" src="../../images/southKorea/${southKoreaLandmarksObj.img}" alt="Celebrity" /></div>
             <h3 class= "landmarks-name">${southKoreaLandmarksObj.name}</h3>
             <p class= "landmarks-desc">${southKoreaLandmarksObj.desc}</p>
-            <p class= "landmarks-city">The closest city to ${southKoreaLandmarksObj.name} is:${southKoreaLandmarksObj.closestCity}</p>
+            <p class= "landmarks-city">The closest city to ${southKoreaLandmarksObj.name} is: ${southKoreaLandmarksObj.closestCity}</p>
         </article>`
     );
 };
