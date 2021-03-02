413de8f44b02ecb16e58e9c5c0ab38463f5dafeb
Cody_Jones <cbjones93@gmail.com>
2021-03-02 11:52:52 -0600

    fixed images :)

--- scripts/nepal/nepalCelebrities/nepalCelebritiesList.js
+++ scripts/nepal/nepalCelebrities/nepalCelebritiesList.js
@@ -1,8 +1,8 @@
 
 export const nepalCelebrities = (nepalCelebritiesObj) => {
     return(
-        `<article class="celebrity-card>
-        <div><img class="celebrity-image" src ="../../images/nepal/${nepalCelebritiesObj.img}" alt="celebrity"/></div>
+        `<article class="celebrity-card">
+        <div><img class="celebrity-image" src ="../../../images/nepal/${nepalCelebritiesObj.img}" alt="celebrity"/></div>
         <h3 class ="celebrity-name">${nepalCelebritiesObj.name}</h3>
         <p class ="celebrity-bio">${nepalCelebritiesObj.bio}</p>
         <p class = "celebrity-job">${nepalCelebritiesObj.job}</p>
